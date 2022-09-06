fc704f4297a2dc51a9353c96ac177353d0a65155
shadow-light <noreply@gracious.tech>
2022-09-06 15:44:14 +1000

    rm deploy code

--- deploy/setup.py
+++
@@ -1,9 +0,0 @@
-
-from setuptools import setup, find_packages
-
-setup(
-    name='deploy',
-    version='0.0.0',
-    packages=find_packages(),
-    console_scripts=['deploy = deploy.cli:main'],
-)
