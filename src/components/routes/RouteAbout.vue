2f0ac40e90270538c460b97f38bd1c0333b6bb35
shadow-light <noreply@gracious.tech>
2023-03-29 22:05:20 +1100

    lcc in about page

--- src/components/routes/RouteAbout.vue
+++ src/components/routes/RouteAbout.vue
@@ -8,7 +8,11 @@ div
         v-toolbar-title About
 
     app-content(class='pa-5')
-        p This app is a free gift from <a href='https://gracious.tech' target='_blank'>Gracious Tech</a>. We create apps that are:
+        p This app is a free gift from <a href='https://gracious.tech' target='_blank'>Gracious Tech</a>.
+        p
+            a(href='https://copy.church/licenses/free-equiv/' target='_blank')
+                img(src='https://copy.church/badges/brand/lcc/free-equiv.svg' alt="Let's copy, church. Public domain equivalent.")
+        p We create apps that are:
         ul(class='mb-3')
             li Mission-focused
             li High quality
@@ -17,13 +21,6 @@ div
         p As such, this app is completely non-profit with no ads, no tracking, and nothing else that would benefit us. This app is for your benefit alone. Your data is stored in your device and never leaves it. You can see this for yourself as <a href='https://github.com/gracious-tech/track' target='_blank'>the code is open source</a>.
         p(class='body-2 text--secondary') Our servers do store data your device sends to us by default (such as your IP address and browser version). Error reports are also sent to us in the rare event the app fails, but they do not contain any of your data. All received data is used for quality improvement, never for commercial reasons.
         p We hope this app helps you read all of God's word and to have faith in him.
-        p
-            v-btn(href='https://github.com/gracious-tech/track' target='_blank' color='accent' text)
-                | Source Code
-                app-svg(name='icon_open_in_new' class='pl-1')
-            v-btn(href='https://gracious.tech' target='_blank' color='accent' text)
-                | gracious.tech
-                app-svg(name='icon_open_in_new' class='pl-1')
 
         p(class='body-2 text--secondary text-center mt-12') Version {{ version }}
         p(class='body-2 text--secondary text-center') {{ sw_status }}
